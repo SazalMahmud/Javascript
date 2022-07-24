dbcc2bd0b0dbea124b77976d5fd60f4ba46d331b
Sazal Mahmud <52792630+SazalMahmud@users.noreply.github.com>
2022-07-24 22:09:13 +0600

    programming

---
+++ timeout.js
@@ -0,0 +1,12 @@
+
+function exicute(){
+    console.log('ma')
+}
+console.log('sazal');
+console.log('shamim');
+setTimeout(exicute,5000);
+console.log('monir');
+console.log('tumpa');
+setTimeout(()=>{
+  console.log('baba');
+},10000);
